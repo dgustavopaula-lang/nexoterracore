730796f61a9c624a594863c3abcb4144108f94c3
Gustavo Santos <dgustavopaula@gmail.com>
2026-09-08 12:12:29 -0300

    Adicionar service worker do Turing no subdomínio

---
+++ service-worker.js
@@ -0,0 +1,44 @@
+const CACHE = "turing-pwa-v2";
+
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./sala-de-comando/turing/style.css",
+  "./sala-de-comando/turing/app.js",
+  "./sala-de-comando/turing/manifest.json",
+  "./sala-de-comando/icons/nexoterracore-192.png",
+  "./sala-de-comando/icons/nexoterracore-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
+  if (url.pathname.startsWith("/api/")) return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        if (response.ok) {
+          const copy = response.clone();
+          caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+        }
+        return response;
+      })
+      .catch(() => caches.match(event.request).then((cached) => cached || caches.match("./index.html")))
+  );
+});
