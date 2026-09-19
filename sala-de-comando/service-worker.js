b24c212b5b7db5553613cda3abbc629587d74b80
Gustavo Santos <dgustavopaula@gmail.com>
2026-09-19 08:52:18 -0300

    hotfix: limpar cache da PWA apos remover login

--- sala-de-comando/service-worker.js
+++ sala-de-comando/service-worker.js
@@ -1,52 +1,18 @@
-const CACHE = "nexoterracore-mobile-v2";
+const CACHE_VERSION = "nexoterracore-login-removed-20260919-0851";
 
-const APP_SHELL = [
-  "./",
-  "./painel.html",
-  "./manifest.json",
-  "./css/style.css",
-  "./js/app.js",
-  "./js/financeiro.js",
-  "./js/clientes.js",
-  "./js/mapas.js",
-  "./js/turing.js",
-  "./js/admin-control.js",
-  "./ntcoins-wallet.js",
-  "./ntcoins-wallet.css",
-  "./icons/nexoterracore-192.png",
-  "./icons/nexoterracore-512.png"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(APP_SHELL))
-  );
+self.addEventListener("install", () => {
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(key => key !== CACHE).map(key => caches.delete(key))
-      )
-    )
+    caches.keys()
+      .then(keys => Promise.all(keys.map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
   );
-  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
-
-  event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        if (response.ok && event.request.url.startsWith(self.location.origin)) {
-          const copia = response.clone();
-          caches.open(CACHE).then(cache => cache.put(event.request, copia));
-        }
-        return response;
-      })
-      .catch(() => caches.match(event.request))
-  );
+  event.respondWith(fetch(event.request));
 });
